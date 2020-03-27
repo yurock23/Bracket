8adbc073b49900264ddb43071eb8ff25707246c4
dauinh <62679553+dauinh@users.noreply.github.com>
2020-03-27 12:01:53 +0700

    Add files via upload
    modified checkBrackets: 
    - convert string to character array
    - push opening brackets in a stack
    - pop opening brackets if it matches the next closing bracket
    - test

--- BracketMatcher.java
+++ BracketMatcher.java
@@ -1,13 +1,16 @@
-public class BracketMatcher{
+public class BracketMatcher {
+
+    private char[] open = {'{','(','[','<'};
+    public SinglyLinkedStack<Character> S = new SinglyLinkedStack<>();
 
     public boolean isOpeningBracket(char c){
-        if(c == '('|| c== "["|| c == '{'|| c  == '<')
+        if(c == '('|| c == '['|| c == '{'|| c  == '<')
             return true;
         return false;
     }
 
     public boolean isClosingBracket(char c){
-        if(c == ')'|| c== "]"|| c == '}'|| c  == '>')
+        if(c == ')'|| c == ']'|| c == '}'|| c  == '>')
             return true;
         return false;
     }
@@ -18,11 +21,33 @@ public class BracketMatcher{
     }
 
     public boolean checkBrackets(String s){
-
-
+        char[] chars = s.toCharArray();             // converts string to array of characters
+        if (chars.length == 0) return false;        // if string is empty
+        for (int i=0; i < chars.length; i++){
+            if (isOpeningBracket(chars[i])) {
+                S.push(chars[i]);
+            }
+            if (isClosingBracket(chars[i])) {
+                if (S.size() == 0) return false;        // there is no opening bracket left in the stack
+                else if (corresponds(S.top(), chars[i])) {
+                    S.pop();
+                }
+            }
+            //S.display();
+        }
+        if (S.size() != 0) return false;        // there is still opening brackets in the stack
+        return true;
     }
 
     public static void main(String[] args){
+        BracketMatcher check = new BracketMatcher();
 
-        
+        String randum1 = "(([])";
+        String randum2 = "}";
+        String randum3 = "{(())}[()]";
+        System.out.println(check.checkBrackets(randum3));
+        System.out.println(check.checkBrackets(randum2));
+        System.out.println(check.checkBrackets(randum1));
+
     }
+}
