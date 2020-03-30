02cdb8b4d9189cef73a8dedb3a0e00875114fbd2
yurock23 <39971224+yurock23@users.noreply.github.com>
2020-03-29 21:32:34 -0400

    Add files via upload

--- BracketMatcher.java
+++ BracketMatcher.java
@@ -1,34 +1,71 @@
 public class BracketMatcher{
-    private char[] open = {'{','(','[','<'};
+    
+    private char[] openArray = {'{','(','[','<'};
+    private char[] closeArray = {'}',')',']','>'};
+    
     public SinglyLinkedStack<Character> S = new SinglyLinkedStack<>();
 
+    /**
+     * Checks if the character is a opening Bracket
+     * @param c character
+     * @return true if the character is an opening bracket false if not
+     */
     public boolean isOpeningBracket(char c){
-        if(c == '('|| c == '['|| c == '{'|| c  == '<')
-            return true;
+        for(char brackets : openArray)      //loops through the array 
+            if(brackets == c)           // check if the bracket matches
+                return true;
         return false;
     }
-
+     /**
+     * Checks if the character is a closing Bracket
+     * @param c character
+     * @return true if the character is an closing bracket false if not
+     */
     public boolean isClosingBracket(char c){
-        if(c == ')'|| c == ']'|| c == '}'|| c  == '>')
-            return true;
+        for(char brackets : closeArray)     //loops through the array 
+            if(brackets == c)       // check if the bracket matches
+                return true;
         return false;
     }
-    public boolean corresponds(char open, char close){
-        if((open == '(' && close == ')')||(open == '[' && close == ']')|| (open == '{' && close == '}' || open == '<' && close == '>'))
-            return true;
-        return false;
+    /**
+     * Finds the index of the value inside the array
+     * @param array array full of characters
+     * @param valueToFind the value that your trying to find the index of
+     * @return it returns the index of the value
+     */
+    public int indexOf(char[] array, int valueToFind){
+        int index = -1;              //inital index
+        for(int i = 0; i<array.length; i++){
+            if(array[i] == valueToFind){            //checks through the array and finds the value
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
-    public boolean checkBrackets(String s){
+
+    public boolean corresponds(char open, char close){
+        int openIndex = indexOf(openArray,open);
+        int closeIndex = indexOf(closeArray,close);
+        if(openIndex == closeIndex)
+            return true;
+        return false;
+    }
+    /**
+     * 
+     * @param s
+     * @return
+     */
+    public boolean checkBrackets(String s) {
         char[] chars = s.toCharArray();             // converts string to array of characters
         char[] brackets = new char[s.length()];
         int j = 0;
         for(char c : chars){
-            if(c == '('|| c == '['|| c == '{'|| c  == '<'||c == ')'|| c == ']'|| c == '}'|| c  == '>')
+            if(isOpeningBracket(c)||isClosingBracket(c))
                 brackets[j] = c;
                 j++;
         }
-
         
         if (chars.length == 0) return false;        // if string is empty
         for (int i=0; i < brackets.length; i++){
@@ -53,6 +90,7 @@ public class BracketMatcher{
         String randum1 = "(([s])";
         String randum2 = "s}";
         String randum3 = "{((d)e)}[()]";
+        System.out.println(check.isOpeningBracket('('));
         System.out.println(check.checkBrackets(randum3));
         System.out.println(check.checkBrackets(randum2));
         System.out.println(check.checkBrackets(randum1));
